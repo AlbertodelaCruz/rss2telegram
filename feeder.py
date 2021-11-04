e110c70308ec5ed810b7116028bee7aaf1d2e16e
Alberto <acruz@alea-soluciones.com>
2021-11-04 07:40:37 +0100

    Check tags in order to avoid false positives

--- feeder.py
+++ feeder.py
@@ -9,7 +9,9 @@ class Feeder:
     def __init__(self, env_loader):
         env_loader.load_dotenv()
         self.blog_url = os.getenv('BLOG_URL')
-        self.feed_authors = os.getenv('FEED_AUTHORS')
+        self.feed_author = os.getenv('FEED_AUTHOR')
+        self.english_feed_author = os.getenv('ENGLISH_FEED_AUTHOR')
+        self.english_feed_tag = os.getenv('ENGLISH_FEED_TAG')
         self.bot_token = os.getenv('BOT_TOKEN')
         self.bot_chatID = os.getenv('BOT_CHATID')
 
@@ -20,7 +22,7 @@ class Feeder:
         new_entries = []
         for entry in sorted_entries:
             published_time = datetime.strptime(entry['published'], "%a, %d %b %Y %H:%M:%S %z" )
-            if published_time > old_messages_datetime and entry['author'] in self.feed_authors:
+            if published_time > old_messages_datetime and (entry['author'] == self.feed_author or (entry['author'] == self.english_feed_author and self.english_feed_tag in entry['tags'][0].values())):
                 old_messages_datetime = published_time
                 new_entries.append(entry)
         return old_messages_datetime, new_entries
