726e1d14fb00482a3c8e1467cd1da9c5f81967a1
Alberto <acruz@alea-soluciones.com>
2022-01-22 19:56:19 +0100

    [FIX] Test

--- specs/rss_to_telegram_spec.py
+++ specs/rss_to_telegram_spec.py
@@ -3,6 +3,7 @@ from doublex import Spy, when, ANY_ARG
 from expects import expect
 from doublex_expects import have_been_called_with
 
+from specs import object_mother
 from use_case.send_new_entries import SendNewEntries
 from infrastructure.twitter import Twitter
 from infrastructure.blog import Blog
@@ -16,14 +17,14 @@ with description('App rss_to_telegram'):
             self.feeder = Spy(Blog)
             self.a_datetime = 'a_datetime'
             self.last_entry_datetimes = {'feed': self.a_datetime, 'twitter': self.a_datetime}
-            self.a_feed_entry = {'title': 'a_title'}
-            self.a_twitter_entry = FakeTwitterEntry()
+            self.a_feed_publication = object_mother.a_publication(date=self.a_datetime)
+            self.a_twitter_publication = object_mother.a_publication(date=self.a_datetime)
 
             self.send_new_entries_use_case = SendNewEntries(self.my_logger, self.last_entry_service, self.twitter,
                                                        self.feeder, self.last_entry_datetimes)
 
-            when(self.feeder).get_new_publications(ANY_ARG).returns(('a_date', [self.a_feed_entry]))
-            when(self.twitter).get_new_publications(ANY_ARG).returns(('a_date', [self.a_twitter_entry]))
+            when(self.feeder).get_new_publications(ANY_ARG).returns(('a_date', [self.a_feed_publication]))
+            when(self.twitter).get_new_publications(ANY_ARG).returns(('a_date', [self.a_twitter_publication]))
             when(self.last_entry_service).get_file(ANY_ARG).returns('/dev/null')
 
 
@@ -31,24 +32,20 @@ with description('App rss_to_telegram'):
         with it('retrieves new feeds'):
             self.send_new_entries_use_case.send()
 
-            expect(self.feeder.get_new_entries).to(have_been_called_with(self.a_datetime))
+            expect(self.feeder.get_new_publications).to(have_been_called_with(self.a_datetime))
 
         with it('retrieves new tweets'):
             self.send_new_entries_use_case.send()
 
-            expect(self.twitter.get_new_entries).to(have_been_called_with(self.a_datetime))
+            expect(self.twitter.get_new_publications).to(have_been_called_with(self.a_datetime))
 
         with it('sends to telegram new feeds'):
             self.send_new_entries_use_case.send()
 
-            expect(self.feeder.send_entries).to(have_been_called_with([self.a_feed_entry]))
+            expect(self.feeder.send_publications).to(have_been_called_with([self.a_feed_publication]))
 
         with it('sends to telegram new tweets'):
             self.send_new_entries_use_case.send()
 
-            expect(self.twitter.send_entries).to(have_been_called_with([self.a_twitter_entry]))
+            expect(self.twitter.send_publications).to(have_been_called_with([self.a_twitter_publication]))
 
-
-class FakeTwitterEntry():
-    def __init__(self):
-        self._json = {'text': 'a_text'}
