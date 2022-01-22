10b13a7fc5b659ee5b054cf4c42c584b785d543c
Alberto <acruz@alea-soluciones.com>
2022-01-22 20:14:40 +0100

    [REFACTOR][TEST] Extract common variables

--- specs/blog_spec.py
+++ specs/blog_spec.py
@@ -19,11 +19,12 @@ with description('Feeder service') as self:
             when(env_loader).load_dotenv().returns(load_dotenv('./system/.env.test'))
             self.feedparser = Spy()
             self.feeder = Blog(env_loader, self.feedparser)
+            self.published_date = 'Fri, 29 Oct 2022 13:06:57 +0000'
+            self.published_entry = datetime.strptime(self.published_date, "%a, %d %b %Y %H:%M:%S %z")
 
         with context('no matching tags'):
             with it('returns saved datetime and empty list'):
-                published_entry = datetime.strptime('Fri, 29 Oct 2022 13:06:57 +0000', "%a, %d %b %Y %H:%M:%S %z" )
-                a_feed = object_mother.a_feed('an_author', 'a_custom_tag', 'Fri, 29 Oct 2022 13:06:57 +0000')
+                a_feed = object_mother.a_feed('an_author', 'a_custom_tag', self.published_date)
                 when(self.feedparser).parse(ANY_ARG).returns({'entries': [a_feed]})
                 last_entry_datetime = object_mother.now()
 
@@ -35,13 +36,11 @@ with description('Feeder service') as self:
             with it('returns last published datetime and publication'):
                 tags_permitted = os.getenv('FEED_TAGS')
                 a_tag_permitted = tags_permitted.split(',')[0]
-                published_date = 'Fri, 29 Oct 2022 13:06:57 +0000'
-                published_entry = datetime.strptime(published_date, "%a, %d %b %Y %H:%M:%S %z" )
-                a_feed = object_mother.a_feed(os.getenv('FEED_AUTHOR'), a_tag_permitted, published_date)
-                a_publication = object_mother.a_publication(date=published_entry)
+                a_feed = object_mother.a_feed(os.getenv('FEED_AUTHOR'), a_tag_permitted, self.published_date)
+                a_publication = object_mother.a_publication(date=self.published_entry)
                 when(self.feedparser).parse(ANY_ARG).returns({'entries': [a_feed]})
                 last_entry_datetime = object_mother.now()
 
                 result = self.feeder.get_new_publications(last_entry_datetime)
 
-                expect(result).to(equal((published_entry, [a_publication])))
+                expect(result).to(equal((self.published_entry, [a_publication])))
