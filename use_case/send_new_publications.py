c5e04c2e6fc348eaeff1acabf5b01b7b80ac83d8
Alberto <acruz@alea-soluciones.com>
2022-01-23 16:08:31 +0100

    [REFACTOR] Naming

--- use_case/send_new_publications.py
+++ use_case/send_new_publications.py
@@ -8,18 +8,18 @@ class SendNewPublications:
         self._telegram_notifier_service = telegram_notifier_service
 
     def send(self):
-        self._my_logger.info("Getting new entries...")
+        self._my_logger.info("Getting new publications...")
         self._last_entry_datetimes['feed'], new_feed_publications = self._blog_publication_service.get_new_publications(self._last_entry_datetimes['feed'])
         self._last_entry_datetimes['twitter'], new_twitter_publications = self._twitter_publication_service.get_new_publications(self._last_entry_datetimes['twitter'])
 
         for new_publication in new_feed_publications:
-            self._my_logger.info(f"New feed entrie: {new_publication.title}")
+            self._my_logger.info(f"New feed publication: {new_publication.title}")
         for new_publication in new_twitter_publications:
-            self._my_logger.info(f"New twitter entrie: {new_publication.content}")
+            self._my_logger.info(f"New twitter publication: {new_publication.content}")
 
         self._last_entry_service.save_file(self._last_entry_datetimes)
 
-        self._my_logger.info("Sending new entries...")
+        self._my_logger.info("Sending new publications...")
         self._telegram_notifier_service.send_publications(new_feed_publications)
         self._telegram_notifier_service.send_publications(new_twitter_publications)
-        self._my_logger.info("Entries sent...")
+        self._my_logger.info("Publications sent...")
