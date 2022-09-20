9c7c1a80a87d4d9afdd457a544ea3b076f8f1d44
Alberto <acruz@alea-soluciones.com>
2022-09-20 22:54:08 +0200

    [REFACTOR] Extract logic to private methods

--- use_case/send_new_publications.py
+++ use_case/send_new_publications.py
@@ -10,18 +10,22 @@ class SendNewPublications:
         self._last_board_message_service = last_board_message_service
 
     def send(self):
+        last_board_message, publications = self._retrieve_publications_from_source()
+        self._last_entry_service.save_file(self._last_entry_datetimes)
+        self._last_board_message_service.save(last_board_message)
+        self._send_publications(publications)
+
+    def _send_publications(self, publications):
+        self._my_logger.info("Sending new publications...")
+        self._telegram_notifier_service.send_publications(publications)
+        self._my_logger.info("Publications sent...")
+
+    def _retrieve_publications_from_source(self):
         self._my_logger.info("Getting new publications...")
         self._last_entry_datetimes['feed'], new_feed_publications = self._blog_publication_service.get_new_publications(self._last_entry_datetimes['feed'])
         self._last_entry_datetimes['twitter'], new_twitter_publications = self._twitter_publication_service.get_new_publications(self._last_entry_datetimes['twitter'])
         last_board_message, new_board_message_publications = self._blog_board_message_publication_service.get_new_publications(self._last_board_message_service.get_message())
         publications = new_feed_publications + new_twitter_publications + new_board_message_publications
-
         for publication in publications:
             self._my_logger.info(f"New publication: {publication.title or publication.content}")
-
-        self._last_entry_service.save_file(self._last_entry_datetimes)
-        self._last_board_message_service.save(last_board_message)
-
-        self._my_logger.info("Sending new publications...")
-        self._telegram_notifier_service.send_publications(publications)
-        self._my_logger.info("Publications sent...")
+        return last_board_message, publications
